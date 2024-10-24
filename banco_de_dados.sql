65b30f33e4495280c3eeb7e5408d064a344c6ac2
RAFAEL CARRENHO TARANTO <rafael.ctaranto@senacsp.edu.br>
2024-10-24 11:29:01 -0300

    novo banco

--- banco_de_dados.sql
+++ banco_de_dados.sql
@@ -1,63 +1,43 @@
 drop database consultorioDB;
 create database consultorioDB;
 use consultorioDB;
-CREATE TABLE cliente (
-    id_cliente INT PRIMARY KEY AUTO_INCREMENT,
+
+CREATE TABLE paciente (
+    id_paciente INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    telefone VARCHAR(15) NOT NULL
+    rg varchar(11) not null unique,
+    telefone VARCHAR(15) 
 );
 
 CREATE TABLE login(
     id_login INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
-    tipo_usuario ENUM('admin', 'dentista') NOT NULL,
-    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-CREATE TABLE funcionario (
-	id_funcionario INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
-    especialidade VARCHAR(255) NOT NULL,
-    contato varchar(15),
-	id_login int,
-	foreign key (id_login) references login(id_login) on delete set null
+    senha VARCHAR(255) NOT NULL
 );
 CREATE TABLE agendamento (
     id_agendamento INT PRIMARY KEY AUTO_INCREMENT,
-    id_cliente INT,
-    id_funcionario INT,
-	data_agendamento DATETIME NOT NULL,
-    status ENUM('confirmado', 'cancelado') NOT NULL,
-    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente) on delete set null,
-    FOREIGN KEY (id_funcionario) REFERENCES funcionario(id_funcionario) on delete set null
+    id_paciente INT,
+	dt_agendamento DATE NOT NULL,
+    hr_agendamento time not null,
+    tratamento varchar(255) not null,
+    status ENUM('confirmado', 'cancelado', 'concluído') NOT NULL,
+    FOREIGN KEY (id_paciente) REFERENCES paciente(id_paciente) on delete set null
 );
-CREATE TABLE estoque (
-	id_item INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
-    quantidade INT NOT NULL,
-    categoria VARCHAR(225) NOT NULL
-);
-INSERT INTO estoque (nome, quantidade, categoria) VALUES 
-('Escova de Dentes', 100, 'Higiene Oral'),
-('Fio Dental', 50, 'Higiene Oral'),
-('Creme Dental', 75, 'Higiene Oral'),
-('Luvas de Procedimento', 200, 'Equipamento'),
-('Máscara Cirúrgica', 150, 'Equipamento'),
-('Desinfetante', 30, 'Materiais de Limpeza'),
-('Algodão', 100, 'Materiais de Limpeza'),
-('Anestésico Local', 25, 'Medicamentos'),
-('Lente de Contato para Câmera', 10, 'Equipamento'),
-('Aparelho de Raios X', 5, 'Equipamento');
+insert into login (nome, email, senha) values ("Rodrigo", "r@gmail.com", sha2("12345678", 256));
+select id_login as ID, nome as Nome, email as Email, senha as Senha from login;
+insert into paciente (nome, rg, telefone) values 
+	("Maria Oliveira dos Santos", "55834758-10", "11 90000-0000"),
+    ("João Carvalho Leão", "55434758-07", "11 98000-0000"),
+    ("Charlie Cernov Amarante", "55856788-02", "11 97000-0000");
+select nome as nome, rg as RG, telefone as telefone from paciente;
 
-INSERT INTO funcionario (nome, especialidade, contato) VALUES
-('Marco Melo', 'Odontopediatria', '11971235674'),
-('Aline Taranto','Endodontia','11995754062'),
-('Filipo Amarante', 'Ortodontia', '11945673434'),
-('Ana Araujo', 'Periodontia', '11990903452');
-
-
+insert into agendamento (id_paciente, dt_agendamento, hr_agendamento, tratamento, status) values 
+(1, "2024-10-30", "14:00:00", "checkup", 'confirmado'),
+(2, "2024-10-20", "11:00:00", "checkup", 'concluído'),
+(3, "2024-10-30", "12:00:00", "canal", 'cancelado');
+select * from agendamento;
+select id_login as id, email from login where email = "r@gmail.com" and senha = sha2("12345678", 256);
 
 
 
