dc2b98fb23a12ef346fb4d7b4cf4fecc0f3866f6
RAFAEL CARRENHO TARANTO <rafael.ctaranto@senacsp.edu.br>
2024-11-06 09:28:39 -0300

    env fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import criarEndpoints from './routes.js'
-
+import dotenv from 'dotenv'
 const server = express()
-
+dotenv.config()
 server.use(cors())
 server.use(express.json())
 
 criarEndpoints(server)
 
-server.listen(5040, console.log("Running on port 5040..."))
+server.listen(5040, console.log("\nRunning on port 5040...\n"))
--- src/repositories/connection.js
+++ src/repositories/connection.js
@@ -1,16 +1,24 @@
 import mysql from "mysql2/promise"
-import 'dotenv/config.js'
-
-
+import dotenv from 'dotenv'
+dotenv.config()
 
 const con = await mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PWD,
     database: process.env.MYSQL_DB,
-    port: process.env.MYSQL_PORT
+    port: process.env.MYSQL_PORT,
+    typeCast: function (field, next) {
+        if (field.type === 'TINY' && field.length === 1) {
+          return field.string() === "1"; 
+        }
+        if (field.type.includes ('DECIMAL')) {
+          return Number(field.string()); 
+        }
+        return next(); 
+    }
 })
 
-console.log("mysql connected")
+console.log("\nmysql connected\n")
 
 export default con;
